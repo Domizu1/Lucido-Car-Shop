88a46d258e6db24e3e9fa47085fa11b50ee71ada
Domizu1 <savadumancic2@gmail.com>
2026-03-07 17:23:54 +0100

    typo fix v3

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Productcard.scss';
+import './productcard.scss';
 import cart from '../assets/shop/cart.svg';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToCart } from '../store/cart';

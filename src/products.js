d5944a65dee719c9c2e1a05bfdd20631704db6f8
Domizu1 <savadumancic2@gmail.com>
2026-03-14 18:29:11 +0100

    wash paste added & mobile fix v3

--- src/products.js
+++ src/products.js
@@ -111,7 +111,7 @@ import thinmedium from './assets/shop/THIN Medium.webp';
 import thinonestep from './assets/shop/THIN One Step.webp';
 import heavycut from './assets/shop/Ultracoat Heavy Cut.webp';
 import mediumcut from './assets/shop/UltraCoat Medium.webp';
-import finecut from './assets/shop/Ultracoat Fine Cut.webp';
+import finecut from './assets/shop/UltraCoat Fine Cut.webp';
 
 
 export const products = [
